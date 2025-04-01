50c788c5585da31b019609b42b0855fc3e50ea66
Dimitar Pashev <dim.pashev@gmail.com>
2025-04-01 16:44:18 +0300

    Delete deploy_django directory

--- deploy_django/src/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", views.hello_world, name="hello_world"),
-    path("health/", views.health_check, name="health_check"),
-]
