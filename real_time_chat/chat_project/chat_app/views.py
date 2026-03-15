0ea3293cff341fd97d62513ae165d6b4820c6d13
Dimitar Pashev <dim.pashev@gmail.com>
2026-03-15 16:39:01 +0200

    add chatroom views

--- real_time_chat/chat_project/chat_app/views.py
+++ real_time_chat/chat_project/chat_app/views.py
@@ -6,3 +6,8 @@ from .models import ChatRoom
 def index(request):
     chat_rooms = ChatRoom.objects.all()
     return render(request, "chat_app/index.html", {"chat_rooms": chat_rooms})
+
+
+def chat_room(request, slug):
+    chat_room = ChatRoom.objects.get(slug=slug)
+    return render(request, "chat_app/room.html", {"chat_room": chat_room})
