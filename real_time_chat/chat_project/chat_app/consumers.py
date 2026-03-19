a97aed2329774acb79dc99d58586d1cda5102b96
Dimitar Pashev <dim.pashev@gmail.com>
2026-03-19 20:33:20 +0200

    send message to socket

--- real_time_chat/chat_project/chat_app/consumers.py
+++ real_time_chat/chat_project/chat_app/consumers.py
@@ -1,3 +1,5 @@
+import json
+
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
@@ -18,4 +20,35 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_discard(
             self.channel_name,
             self.room_group_name,
+        )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data["message"]
+        username = data["username"]
+        room = data["room"]
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "chat.message",
+                "message": message,
+                "username": username,
+                "room": room,
+            },
+        )
+
+    async def chat_message(self, event):
+        message = event["message"]
+        username = event["username"]
+        room = event["room"]
+
+        await self.send(
+            text_data=json.dumps(
+                {
+                    "message": message,
+                    "username": username,
+                    "room": room,
+                }
+            )
         )
